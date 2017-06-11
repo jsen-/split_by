be8e0250062b310d85074153bc273bbbbbc71927
jsen- <max.enhanced@gmail.com>
2017-06-11 17:35:44 +0200

    fix for https://github.com/rust-lang/rust/issues/42465

--- src/lib.rs
+++ src/lib.rs
@@ -103,7 +103,7 @@ impl<'a, R: Read, A: Automaton<&'a [u8]> > Iterator for SplitByIter<'a, R, A> {
 /// second
 /// ********########
 /// third
-/// ################
+/// #################
 /// last"#.split_by(&ac);
 ///
 /// assert!(splits.next().unwrap().unwrap().as_slice() == b"first\n");
